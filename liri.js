00047b49c8eda969a2cc0b0c2e82c02db6a5e68c
Zach Zador <zazador@gmail.com>
2017-04-01 15:15:54 -0500

    spotify working

--- liri.js
+++ liri.js
@@ -21,4 +21,31 @@ if (command === "my-tweets") {
 		}
 
 	});
+} else if (command === "spotify-this-song") {
+	var song = process.argv[3];
+	if (song) {
+		Spotify.search({ type: 'track', query: song }, function(err, data) {
+			if ( err ) {
+				console.log('Error occurred: ' + err);
+				return;
+			}
+
+			console.log("Artist: " + data.tracks.items[0].album.artists[0].name); 
+			console.log("Track: " + data.tracks.items[0].name);
+			console.log("Preview Link: " + data.tracks.items[0].preview_url);
+			console.log("Album: " + data.tracks.items[0].album.name);
+		});
+	} else {
+		Spotify.search({ type: 'track', query: "artist:Ace of Base&track:The Sign" }, function(err, data) {
+			if ( err ) {
+				console.log('Error occurred: ' + err);
+				return;
+			}
+
+			console.log("Artist: " + data.tracks.items[0].album.artists[0].name); 
+			console.log("Track: " + data.tracks.items[0].name);
+			console.log("Preview Link: " + data.tracks.items[0].preview_url);
+			console.log("Album: " + data.tracks.items[0].album.name);
+		});
+	}
 }
