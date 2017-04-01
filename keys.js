c93544511f9c65576843070244ec0310f3af2db4
Zach Zador <zazador@gmail.com>
2017-04-01 10:33:00 -0500

    initial files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '7smraEPmYAYNKGexCM27Kx5wF',
+  consumer_secret: 'HySaafdBCTuDPz1eCSmJMm67PlwEzZUrMkXbZiWt99kfR0bavT',
+  access_token_key: '343230410-WRbyYd78PPmGkzkrsilP6DXpx1uCd6ItT7UPAh0f',
+  access_token_secret: 'uwSum2ztqUOEZvKfZtCXy8Y3RLasXstDzRNNqdkDKj7ko',
+}
